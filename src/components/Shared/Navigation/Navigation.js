0bb9fa660ee2f57517b45b48b516f25a10721280
mohisikder <mohi.info20@gmail.com>
2022-03-28 22:36:12 +0600

    added header top and navbar

--- src/components/Shared/Navigation/Navigation.js
+++ src/components/Shared/Navigation/Navigation.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Navigation.css";
-import { Col, Container, Nav, Navbar, Row } from "react-bootstrap";
+import { Button, Col, Container, Nav, Navbar, Row } from "react-bootstrap";
 import {
-  FaPhoneAlt,
+  FaPhone,
   FaFacebookSquare,
   FaInstagramSquare,
   FaTwitterSquare,
+  FaEnvelope,
 } from "react-icons/fa";
 
 const Navigation = () => {
@@ -15,24 +16,20 @@ const Navigation = () => {
         <Container>
           <Row>
             <Col md={12} xs={12}>
-              <div className="header__top--info d-flex justify-content-between">
-                <div className="header__phone">
-                  <p className="pt-3">
+              <div className="header__top text-white d-flex justify-content-between">
+                <div className="toolbar d-flex align-items-center">
+                  <p className="me-4 have__any">Have any questions?</p>
+                  <p>
                     {" "}
-                    <FaPhoneAlt className="me-2 header-icon" />
-                    +880123456987
+                    <FaPhone className="me-2" />
+                    +8801236 147 258
+                  </p>
+                  <p className="ms-4">
+                    <FaEnvelope className="me-2" /> info@aliacademy.com
                   </p>
                 </div>
-                <div className="header-social pt-3">
-                  <a className="me-3" href="#">
-                    <FaFacebookSquare className="fs-3" />
-                  </a>
-                  <a className="me-3" href="#">
-                    <FaInstagramSquare className="fs-3" />
-                  </a>
-                  <a href="#">
-                    <FaTwitterSquare className="fs-3" />
-                  </a>
+                <div className="login">
+                  <Button>Login</Button>
                 </div>
               </div>
             </Col>
@@ -43,35 +40,36 @@ const Navigation = () => {
         <Navbar
           collapseOnSelect
           expand="lg"
-          variant="dark"
-          style={{ background: "#07d475" }}
+          variant="light"
           shadow-lg
           sticky="top"
         >
           <Container>
-            <Navbar.Brand href="#home">Intementor</Navbar.Brand>
+            <Navbar.Brand href="/">
+              Ali-<span className="logo__color">Academy</span>
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="ms-auto">
-                <Nav.Link className="me-3" href="#features">
+                <Nav.Link className="me-3" href="/">
                   Home
                 </Nav.Link>
-                <Nav.Link className="me-3" href="#pricing">
-                  About us
+                <Nav.Link className="me-3" href="about">
+                  About
                 </Nav.Link>
-                <Nav.Link className="me-3" href="#pricing">
-                  Services
+                <Nav.Link className="me-3" href="courses">
+                  Courses
                 </Nav.Link>
-                <Nav.Link className="me-3" href="#pricing">
-                  Projects
+                <Nav.Link className="me-3" href="features">
+                  Features
                 </Nav.Link>
-                <Nav.Link className="me-3" href="#pricing">
+                <Nav.Link className="me-3" href="events">
+                  Events
+                </Nav.Link>
+                <Nav.Link className="me-3" href="blog">
                   Blog
                 </Nav.Link>
-                <Nav.Link className="me-3" href="#pricing">
-                  Shop
-                </Nav.Link>
-                <Nav.Link href="#pricing">Contact us</Nav.Link>
+                <Nav.Link href="contact">Contact</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
