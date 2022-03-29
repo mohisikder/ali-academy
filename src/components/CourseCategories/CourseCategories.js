9d5756828932688fd7aa383220a814bcf68e8e34
mohisikder <mohi.info20@gmail.com>
2022-03-29 13:20:25 +0600

    corses section added

---
+++ src/components/CourseCategories/CourseCategories.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "./CourseCategories.css";
+
+import FrontEnd from "./../../assets/images/bg-1.jpg";
+
+const CourseCategories = () => {
+  return (
+    <>
+      <div className="course__categories">
+        <Container>
+          <Row xs={12} md={6} className="g-4">
+            {Array.from({ length: 6 }).map((_, idx) => (
+              <Col>
+                <div className="courses__info">
+                  <div className="courses__img">
+                    <img src={FrontEnd} alt="" />
+                  </div>
+
+                  <div className="courses__title">
+                    <h4>FrontEnd </h4>
+                  </div>
+                </div>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default CourseCategories;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
+import CourseCategories from "../CourseCategories/CourseCategories";
+import Courses from "../Courses/Courses";
 import Header from "../Header/Header";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <CourseCategories />
+      <Courses />
     </>
   );
 };
